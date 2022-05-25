411738e9fef992e934b6ec8f4143055e08d24985
JacckietY <80045830+JacckietY@users.noreply.github.com>
2022-05-25 13:19:34 +0300

    added material + icons + fonts. First components were created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { MainPageComponent } from './main-page/main-page.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { ProductCardComponent } from './base-components/product-card/product-card.component';
+import { AccountPageComponent } from './account-page/account-page.component';
+import { LoginPopupComponent } from './base-components/login-popup/login-popup.component';
+import { SigninPopupComponent } from './base-components/signin-popup/signin-popup.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    ShoppingCartComponent,
+    CatalogueComponent,
+    ProductCardComponent,
+    AccountPageComponent,
+    LoginPopupComponent,
+    SigninPopupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
