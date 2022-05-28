02fc24a3b04f3f13293c647903406f7dc232f84e
Lopatin Bohdan <lopatin.bogdan77@gmail.com>
2022-05-28 15:46:26 +0300

    started working on main-page

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
 
 const MaterialComponents = [
   MatButtonModule,
   MatIconModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatFormFieldModule,
+  
 ];
 
 @NgModule({
